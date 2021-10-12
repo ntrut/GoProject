a5d7c02f8fd2154915eb9261348a7cd2d3ef7ce8
ntrut <nazartrut@gmail.com>
2021-10-12 14:42:48 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module example/project
+
+go 1.13
+
+require (
+	github.com/aws/aws-sdk-go v1.41.1
+	github.com/jamespearly/loggly v0.0.0-20190206220007-041558ec1c7c
+	github.com/joho/godotenv v1.4.0
+)
---
+++ project.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/jamespearly/loggly"
+	"github.com/joho/godotenv"
+)
+
+type cryptoData struct {
+	Key struct {
+		Id                string `json:"id"`
+		Rank              string `json:"rank"`
+		Symbol            string `json:"symbol"`
+		Name              string `json:"name"`
+		Supply            string `json:"supply"`
+		MaxSupply         string `json:"maxSupply"`
+		MarketCapUsd      string `json:"marketCapUsd"`
+		VolumeUsd24Hr     string `json:"volumeUsd24Hr"`
+		PriceUsd          string `json:"priceUsd"`
+		ChangePercent24hr string `json:"changePercent24hr"`
+		Vwap24Hr          string `json:"vwap24Hr"`
+		Explorer          string `json:"explorer"`
+	} `json:"data"`
+	//Timestamp int64 `json:"timestamp"`
+}
+
+func goCode(client *loggly.ClientType, tk *time.Ticker, db *dynamodb.DynamoDB, coin string) {
+	//define struct
+	var dataStruct cryptoData
+	resp, err := http.Get("https://api.coincap.io/v2/assets/" + coin)
+	var statusCode int = resp.StatusCode
+	if err != nil {
+		_ = client.Send("error", "Error using the Get request")
+		return
+	} else {
+		if resp.StatusCode != http.StatusOK {
+			_ = client.Send("error", "Error code "+resp.Status)
+		} else {
+			_ = client.Send("info", "200 Status Code, Success")
+		}
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		_ = client.Send("error", "Error reading from the body")
+	}
+
+	//unmarshal the data
+	err = json.Unmarshal(body, &dataStruct)
+	if err != nil {
+		_ = client.Send("error", "Error on unmarshalling from the body")
+	}
+
+	//Print out test data
+	fmt.Println("************************************************")
+	fmt.Println("Struct Display: ", dataStruct)
+	fmt.Println("")
+	fmt.Println("-----------------------------------------------")
+	fmt.Println("ID: ", dataStruct.Key.Id)
+	fmt.Println("Current Price: ", dataStruct.Key.PriceUsd)
+	fmt.Println("************************************************")
+
+	//Loggly stuff when everything is successful
+	//only do this when we receive status code 200
+	if statusCode == http.StatusOK {
+		_ = client.Send("info", "Successfully got the request")
+
+		bodysize := strconv.Itoa(len(body))
+
+		_ = client.Send("info", "Amount of data is: "+bodysize)
+
+		//marshall map the struct
+		av, err := dynamodbattribute.MarshalMap(dataStruct.Key)
+		if err != nil {
+			_ = client.Send("error", "Got error marshalling new movie item")
+		}
+		tableName := "Crypto"
+		//fmt.Println(av)
+		input := &dynamodb.PutItemInput{
+			Item:      av,
+			TableName: aws.String(tableName),
+		}
+
+		_, err = db.PutItem(input)
+		if err != nil {
+			_ = client.Send("error", "Got error calling PutItem")
+			fmt.Println(err)
+			_ = client.Send("info", "Successfully created a db item")
+		}
+
+	}
+}
+
+func main() {
+	err := godotenv.Load("app.env")
+	if err != nil {
+		fmt.Println("Error loading the .env file")
+	}
+
+	// Create an AWS session for US East 1.
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	}))
+
+	// Create a DynamoDB instance
+	db := dynamodb.New(sess)
+
+	//define a client for loggly
+	client := loggly.New("nazartrut")
+
+	//create poll
+	duration := time.Duration(20) * time.Second //every 20 seconds
+
+	tk := time.NewTicker(duration)
+
+	for range tk.C {
+		goCode(client, tk, db, "bitcoin")
+		goCode(client, tk, db, "dogecoin")
+		goCode(client, tk, db, "ethereum")
+	}
+
+}
